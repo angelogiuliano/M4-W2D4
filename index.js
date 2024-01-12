1df71dca4b160b42d26cd19ef3e32c7a84f414e3
ang <angelo03giuliano@gmail.com>
2024-01-12 16:16:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,7 @@ let books = function () {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((res) => res.json())
     .then((data) => {
-
       let listaCarrello = [];
-      let aggiungiAlCarrello = function (elem) {
-        if (!listaCarrello.includes(elem.parentElement.attributes.id.value)) {
-          listaCarrello.push(elem.parentElement.attributes.id.value);
-          console.log("aggiunto");
-        } else {
-          const index = listaCarrello.indexOf(elem.parentElement.attributes.id.value);
-          if (index > -1) {
-            listaCarrello.splice(index, 1);
-          }
-        }
-        if (elem.parentElement.classList.value.includes("bg-success")) {
-          elem.parentElement.classList.remove("bg-success");
-        } else {
-          elem.parentElement.classList.add("bg-success");
-        }
-        console.log(listaCarrello);
-      };
 
       let filtraLibri = function (ricerca) {
         main.innerHTML = "";
@@ -43,8 +25,14 @@ let books = function () {
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                   </svg>
               </button>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                </svg>
+              </button>
           </div>
-                `;
+            `;
         });
       };
 
@@ -61,6 +49,12 @@ let books = function () {
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                     </svg>
                 </button>
+                <button class="not-interested ms-3 mt-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-x-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                  </svg>
+              </button>
             </div>
         `;
       });
@@ -73,11 +67,34 @@ let books = function () {
         });
 
       let cartButtons = document.getElementsByClassName("cart");
+      let notInterestedButtons = document.getElementsByClassName("not-interested");
+
       for (let i = 0; i < cartButtons.length; i++) {
         cartButtons[i].addEventListener("click", function () {
-          aggiungiAlCarrello(this);
+          if (!listaCarrello.includes(this.parentElement.attributes.id.value)) {
+            listaCarrello.push(this.parentElement.attributes.id.value);
+            console.log("aggiunto");
+          } else {
+            const index = listaCarrello.indexOf(this.parentElement.attributes.id.value);
+            if (index > -1) {
+              listaCarrello.splice(index, 1);
+            }
+          }
+          if (this.parentElement.classList.value.includes("bg-success")) {
+            this.parentElement.classList.remove("bg-success");
+          } else {
+            this.parentElement.classList.add("bg-success");
+          }
         });
       }
+
+      for (let i = 0; i < notInterestedButtons.length; i++) {
+        notInterestedButtons[i].addEventListener("click", function () {
+          console.log(this.classList)
+          this.parentElement.classList.add("d-none")
+        });
+      }
+
     });
 };
 
